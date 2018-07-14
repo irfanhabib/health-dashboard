78d19cde53abe0a2406a55719a903cde2fbbc8b7
Irfan Habib <irfan.habib@suse.com>
2018-07-14 11:52:47 +0100

    Fix End of Month

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -2,16 +2,9 @@ export const getEpochs = () => {
     const currentYear = (new Date()).getFullYear();
     let nextYear = currentYear;
     const currentMonth = (new Date()).getMonth() + 1;
-    let nextMonth = currentMonth;
-    if (currentMonth === 12){
-      nextMonth = 1;
-      nextYear += 1;
-    } else {
-      nextMonth = currentMonth + 1;
-    }
     return {
       startOfYear: (new Date(`01-01-${currentYear} 00:00:00`)).getTime(),
       startOfMonth:  (new Date(`${currentMonth}-01-${currentYear} 00:00:00`)).getTime(),
-      endOfMonth:  (new Date(`${nextMonth}-01-${nextYear} 00:00:00`)).getTime()
+      endOfMonth:  new Date().getTime()
     }
 }
